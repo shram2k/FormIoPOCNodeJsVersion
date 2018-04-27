7338fdab21ad7ab98f439cc4d1e7c44d1dc018fb
Ram Kumar Sharma <Ram.Sharma@rsystems.com>
2018-04-27 13:54:34 +0530

    exception handling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 // Define the port to run on
 app.set('port', 3000);
 
-app.use(express.static(path.join(__dirname, 'TalentFirst/TalentFirstV3')));
+app.use(express.static(path.join(__dirname, 'TalentFirst/TalentFirstV3'))); //https://expressjs.com/en/starter/static-files.html
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + 'index.html'));
@@ -15,3 +15,15 @@ var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log('Magic happens on port ' + port);
 });
+
+//handle unhandled exception
+process.on('uncaughtException', (err) => {
+  console.log(err);
+});
+
+// setTimeout(() => {
+//   nonexistentFunc();
+// }, 500);
+
+
+
